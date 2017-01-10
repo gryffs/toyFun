ec83b680b04d7b646462ff9039095ff34b93fdc7
Chad Griffis <cmg123@me.com>
2017-01-10 08:45:25 -0600

    Create and solve allAnagrams.js

---
+++ allAnagrams.js
@@ -0,0 +1,23 @@
+/*
+Given a single input string, write a function that produces all possible anagrams of a string and outputs them as an array.
+*/
+
+function allAnagrams (string) {
+  //  use an object to handle repeats from the recursive call below.
+  const uniqueOutput = {};
+
+  (function anagram (ana, str) { // IIFE FTW!
+    // only save the combinations that use all the letter
+    if (str === '') { uniqueOutput[ana] = 1; }
+
+    for (let i = 0; i < str.length; i++) {
+      // run through the string building every possible combination
+      anagram(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+    }
+  })('', string);
+ 
+  let result = Object.keys(uniqueOutput);
+  return result;
+}
+
+allAnagrams('abc') / => [ "abc", "acb", "bac", "bca", "cab", "cba" ]
